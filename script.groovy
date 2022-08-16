02300701c7e1f678b899fe346ff302a306db5494
yosryrwyhel <80420429+yosryrwyhel@users.noreply.github.com>
2022-08-16 14:03:03 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -10,9 +10,9 @@ def test() {
 def buildImage() {
   echo "building the docker image..."
   withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-     sh 'docker build -t yosryrwyhel/petclinic:pt-1.0 .'
+     sh 'docker build -t yosryrwyhel/petclinic:jma-1.0 .'
      sh "echo $PASS | docker login -u $USER --password-stdin"
-     sh 'docker push yosryrwyhel/petclinic:pt-1.0'
+     sh 'docker push yosryrwyhel/petclinic:jma-1.0'
   }
 }
 
