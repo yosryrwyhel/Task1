b90dad1883fd645016a6f9a3bbe97532419e9a78
yosryrwyhel <80420429+yosryrwyhel@users.noreply.github.com>
2022-08-22 14:57:14 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,6 +8,13 @@ def test() {
   echo 'testing the application...'
 }
 
+def sonarqube() {
+  echo 'testing the application...'
+  withSonarQubeEnv(installationName: 'sq1'){
+    sh './mvnw clean sonar:sonar'
+  }
+}
+
 def buildImage() {
   echo "building the docker image..."
   withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
