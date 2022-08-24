fc09621d205701f7e57ff696af2d9677f80c7172
yosryrwyhel <80420429+yosryrwyhel@users.noreply.github.com>
2022-08-24 17:12:39 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -9,18 +9,18 @@ def test() {
 
 def sonarqube() {
   echo 'testing the application...'
-//   withSonarQubeEnv(installationName: 'sq1'){
-//     sh './mvnw clean sonar:sonar'
-//   }
+  withSonarQubeEnv(installationName: 'sq1'){
+    sh './mvnw clean sonar:sonar'
+  }
 }
 
 def buildImage() {
   echo "building the docker image..."
-//   withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-//      sh 'docker build -t yosryrwyhel/petclinic:jma-1.0 .'
-//      sh "echo $PASS | docker login -u $USER --password-stdin"
-//      sh 'docker push yosryrwyhel/petclinic:jma-1.0'
-//   }
+  withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+     sh 'docker build -t yosryrwyhel/petclinic:jma-1.0 .'
+     sh "echo $PASS | docker login -u $USER --password-stdin"
+     sh 'docker push yosryrwyhel/petclinic:jma-1.0'
+  }
 }
 
 def deployApp() {
